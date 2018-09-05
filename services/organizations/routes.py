6f2ca34cce73cfe28453c35b8044f05bd2515148
Howard Powell <howard.powell@bluespurs.com>
2018-09-05 15:01:37 -0300

    Addition of organization verification endpoint

--- services/organizations/routes.py
+++ services/organizations/routes.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from flask import Blueprint, jsonify
@@ -32,5 +33,28 @@ def add_organization(**kwargs):
 
     response = jsonify(organization_schema.dump(organization).data)
     response.status_code = 201
+
+    return response
+
+
+@blueprint.route('/organizations/<org_id>/verify', methods=["PUT"])
+def verify_organization(org_id):
+    try:
+        # TODO: Create the user record in Cognito and the database
+
+        # Update the verification flag and assign the user to the organization
+        organization = OrganizationModel.get(hash_key=org_id)
+        organization.update(
+            actions=[
+                OrganizationModel.is_verified.set(True)
+            ]
+        )
+
+        response = jsonify(organization_schema.dump(organization).data)
+        response.status_code = 201
+
+    except OrganizationModel.DoesNotExist:
+        response = jsonify({'message': 'Organization {} does not exist'.format(org_id)})
+        response.status_code = 422
 
     return response
