d1ed48ed04fc5787b47f6b6a613e8aa0f7ffdbdd
Howard Powell <howard.powell@bluespurs.com>
2018-09-14 20:55:12 -0300

    Addition of handler to process webargs errors and return a 422 response same as when using version 3.0.2

--- api.py
+++ api.py
@@ -1,3 +1,16 @@
 from core import init
-
+import flask
 app = init.init_application()
+
+
+# Return validation errors from webargs as JSON
+@app.errorhandler(422)
+def handle_unprocessable_entity(err):
+    # webargs attaches additional metadata to the `data` attribute
+    exc = getattr(err, "exc")
+    if exc:
+        # Get validations from the ValidationError object
+        messages = exc.messages
+    else:
+        messages = ["Invalid request"]
+    return flask.jsonify({"messages": messages}), 422
