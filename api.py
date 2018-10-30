b988df58ff914017fc45761199c740bb88e078d5
Howard Powell <howard.powell@bluespurs.com>
2018-10-30 11:18:20 -0300

    Updates to error handling

--- api.py
+++ api.py
@@ -1,25 +1,3 @@
-import flask
-from core import init, errors
+from core import init
 
 app = init.init_application()
-
-
-# Return validation errors from webargs as JSON
-@app.errorhandler(422)
-def handle_unprocessable_entity(err):
-    # webargs attaches additional metadata to the `data` attribute
-    exc = getattr(err, "exc")
-    if exc:
-        # Get validations from the ValidationError object
-        messages = exc.messages
-    else:
-        messages = ["Invalid request"]
-    return flask.jsonify({"messages": messages}), 422
-
-
-# Handle ResourceValidationError messages
-@app.errorhandler(errors.ResourceValidationError)
-def handle_invalid_usage(error):
-    response = flask.jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
--- services/error/routes.py
+++ services/error/routes.py
@@ -12,6 +12,13 @@ def handle_error(error):
     return flask.jsonify(ERROR_SCHEMA.dump(error).data), error.status_code
 
 
+@blueprint.app_errorhandler(errors.ResourceValidationError)
+def handle_invalid_usage(error):
+    return flask.jsonify(error.to_dict()), error.status_code
+
+
 @blueprint.app_errorhandler(errors.UNPROCESSABLE_ENTITY)
 def handle_validation_error(error):
+    error.data.pop('schema', None)  # Remove schema from response
     return flask.jsonify(error.data), errors.UNPROCESSABLE_ENTITY
+
