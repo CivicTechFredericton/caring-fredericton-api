90cd665293da26fb4938436d9503e78acea62958
Howard Powell <howard.powell@bluespurs.com>
2018-11-09 14:49:03 -0400

    WIP Event recurrence changes

--- services/events/constants.py
+++ services/events/constants.py
@@ -1,2 +1,12 @@
+from enum import Enum
+
 EVENT_DATE_FORMAT = '%Y-%m-%d'
 EVENT_TIME_FORMAT = '%H:%M:%S'
+
+
+class OccurrenceType(Enum):
+    ONE_TIME = 1
+    DAILY = 2
+    BI_WEEKLY = 3
+    WEEKLY = 4
+    MONTHLY = 5
--- services/events/resource.py
+++ services/events/resource.py
@@ -3,6 +3,24 @@ from marshmallow import fields
 from services.events import constants
 
 
+# http://www.vertabelo.com/blog/technical-articles/again-and-again-managing-recurring-events-in-a-data-model
+# https://stackoverflow.com/questions/10890480/recurring-events-schema-w-mongodb
+class EventOccurenceSchema(ma.Schema):
+    date = fields.Str()
+
+    class Meta:
+        strict = True
+
+
+class RecurrencyDetails(ma.Schema):
+    num_occurences = fields.Int(default=0)
+    # day_of_week = fields.Int(required=False, validate=lambda val: 1 <= val <= 7)
+    days_of_week = fields.List(fields.Str())
+
+    class Meta:
+        strict = True
+
+
 class EventSchema(ma.Schema):
     id = fields.Str(dump_only=True)
     owner = fields.Str(dump_only=True)
@@ -13,6 +31,15 @@ class EventSchema(ma.Schema):
     start_time = fields.Time(required=True, format=constants.EVENT_TIME_FORMAT)
     end_time = fields.Time(required=True, format=constants.EVENT_TIME_FORMAT)
     timezone = fields.Str(dump_only=True)
+    recurrence = fields.Str(required=True, missing=constants.OccurrenceType.ONE_TIME)
+    recurrence_details = fields.Nested(RecurrencyDetails, required=False)
+
+    class Meta:
+        strict = True
+
+
+class EventDetailsSchema(EventSchema):
+    occurrences = fields.Nested(EventOccurenceSchema, dump_only=True)
 
     class Meta:
         strict = True
