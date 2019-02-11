ce44af576a92b05b32d9fe08f56151b6f88859ff
Howard Powell <howard.powell@bluespurs.com>
2019-02-11 11:52:49 -0400

    Removal of unused handler methods

--- handlers/api_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def set_dynamo_table_name(model):
-    service_name = os.environ['SERVICE_NAME']
-    stage_name = os.environ['STAGE']
-    model.Meta.table_name = \
-        '{}-{}-{}'.format(service_name, stage_name, model.Meta.simple_name)
