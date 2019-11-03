e183b4cfee7dcc044977a2c464c57bc34747d47e
Howard Powell <hpowell@deloitte.ca>
2019-11-03 13:05:46 -0400

    Updated script files

---
+++ launch-env.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+region=$1
+env=$2
+
+echo "------------------------------"
+echo "Installing NPM Dependencies"
+echo "------------------------------"
+echo
+npm ci
+
+echo "------------------------------"
+echo "Installing Python Dependencies"
+echo "------------------------------"
+echo
+pip install -r requirements.txt
+
+echo
+echo "---------------------"
+echo "Deploying API Gateway"
+echo "---------------------"
+echo
+pushd api_gateway
+ln -sf ../node_modules
+npm run deploy -- --region $region --stage $env
+popd
+
+echo
+echo "-----------------------"
+echo "Deploying Dynamo Tables"
+echo "-----------------------"
+echo
+pushd dynamo_tables
+ln -sf ../node_modules
+npm run deploy -- --region $region --stage $env
+popd
+
+echo
+echo "-------------------------"
+echo "Deploying API Application"
+echo "-------------------------"
+echo
+npm run deploy -- --region $region --stage $env
+
+pip install -r requirements.txt
