012f28799aa627e33b4c23de79cef71363f3eb3e
Howard Powell <hspowell@gmail.com>
2021-02-08 16:15:10 -0400

    Fix to missing function call

--- cognito_triggers/handlers/custom_messages.py
+++ cognito_triggers/handlers/custom_messages.py
@@ -1,4 +1,11 @@
-from core.request import generate_web_url
+import os
+
+
+def generate_web_url(path: str) -> str:
+    """
+    Generates a web url, path must start with / or empty string if you only want the web url
+    """
+    return f"{os.getenv('UI_DOMAIN_NAME')}{path}"
 
 
 def handler(event, context):
